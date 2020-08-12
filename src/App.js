006a84cd0d5c57bd165dcdab03e6242e4f52f14e
Jina Zhu <jinazhu87@gmail.com>
2020-08-11 21:21:59 -0700

    fetch results

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [movieResult, setMovieResult] = useState([]);
+  console.log("title", title);
+  console.log("movieResult", movieResult);
+
+  const fetchMovies = async (e) => {
+    e.preventDefault();
+    try {
+      const movies = await fetch(
+        `http://www.omdbapi.com/?apikey=1e9a13fb&s=${title}`
+      );
+      const movieMatch = await movies.json();
+      setMovieResult(movieMatch);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar setTitle={setTitle} title={title} fetchMovies={fetchMovies} />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,15 @@ import search from "./image/search.svg";
 import deleteLogo from "./image/delete.svg";
 import { SearchForm, Button, NavbarContainer, SearchBar, Logo } from "./styled";
 
-const Navbar = () => {
+const Navbar = ({ setTitle, title, fetchMovies }) => {
   return (
     <NavbarContainer>
       <Logo>Netfake</Logo>
       <SearchForm>
-        <Button>
+        <Button onClick={(e) => fetchMovies(e)}>
           <img src={search} alt="search button" width="15" />
         </Button>
-        <SearchBar type="text" />
+        <SearchBar type="text" onChange={(e) => setTitle(e.target.value)} />
         <Button>
           <img src={deleteLogo} alt="search button" width="10" />
         </Button>
