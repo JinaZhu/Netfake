58902095bf8f7f42b67629c929229efab813b6a7
Jina Zhu <jinazhu87@gmail.com>
2020-08-16 20:49:25 -0700

    enable more results

--- src/App.js
+++ src/App.js
@@ -11,29 +11,49 @@ const apiKey = config.omdgAPIKey;
 function App() {
   const [title, setTitle] = useState("");
   const [movieResult, setMovieResult] = useState([]);
-  console.log("title", title);
-  console.log("movieResult", movieResult);
+  const [count, setCount] = useState(1);
 
-  const fetchMovies = async (e, page = 1) => {
+  const fetchMovies = async (e, page) => {
     e.preventDefault();
     try {
       const movies = await fetch(
         `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}&page=${page}`
       );
       const movieMatch = await movies.json();
-      setMovieResult(movieMatch.Search);
-      setTitle("");
+      console.log(movieMatch);
+      if (movieResult.length === 0) {
+        setMovieResult(movieMatch.Search);
+      } else {
+        let movieResultCopy = [...movieResult];
+        console.log("new results", movieMatch.Search);
+        const updatedMovieResult = movieResultCopy.concat(movieMatch.Search);
+        // console.log("updateResult", updateResult);
+        setMovieResult(updatedMovieResult);
+      }
+      // setTitle("");
+      let addCount = count + 1;
+      setCount(addCount);
     } catch (error) {
       console.log(`Error: ${error}`);
     }
   };
   console.log(movieResult);
+  console.log("count", count);
 
   return (
     <div className="App">
-      <Navbar setTitle={setTitle} title={title} fetchMovies={fetchMovies} />
+      <Navbar
+        setTitle={setTitle}
+        title={title}
+        fetchMovies={fetchMovies}
+        count={count}
+      />
       <Results movieResult={movieResult} />
-      {movieResult.length >= 1 && <LoadButton>Load More</LoadButton>}
+      {movieResult.length >= 1 && (
+        <LoadButton onClick={(e) => fetchMovies(e, count)}>
+          Load More
+        </LoadButton>
+      )}
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,12 +3,12 @@ import search from "./image/search.svg";
 import deleteLogo from "./image/delete.svg";
 import { SearchForm, Button, NavbarContainer, SearchBar, Logo } from "./styled";
 
-const Navbar = ({ setTitle, title, fetchMovies }) => {
+const Navbar = ({ setTitle, title, fetchMovies, count }) => {
   return (
     <NavbarContainer>
       <Logo>Netfake</Logo>
       <SearchForm>
-        <Button onClick={(e) => fetchMovies(e)}>
+        <Button onClick={(e) => fetchMovies(e, count)}>
           <img src={search} alt="search button" width="15" />
         </Button>
         <SearchBar
